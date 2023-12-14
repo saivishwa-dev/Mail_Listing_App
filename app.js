b45deb0ecb5279df71434b404d70415d8c155ced
saivishwa-dev <saivishwa692@gmail.com>
2023-12-14 20:35:19 +0530

    updated get request

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const https = require('https');
+const axios = require('axios');
 const favicon = require('serve-favicon');
 const path  = require('path')
+const api_key = "b76f30b929ffbae19e66a56b9f37d59b-us21";
+const listId = "5ad9987946";
 const PORT  = 3000;
 
 const app = express();
     
+const mailChimAPI = axios.create({
+    baseURL: 'https://us21.api.mailchimp.com/3.0',
+    auth: {
+        username: 'Anystring',
+        password: api_key,
+    },
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -19,6 +28,10 @@ app.get("/",(req,res)=> {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
 
+app.post("/", (req,res)=> {
+    
+
+})
 app.listen(PORT, ()=>{
     console.log(`Server is Started and Running at PORT ${PORT}`)
 })
